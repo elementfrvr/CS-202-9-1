42bdbcf31a0c13e0d530f0eed53ee4abe81ca6db
elementfrvr <dakota.mitchem@siu.edu>
2015-12-18 01:30:09 -0600

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,8 @@
+public class Employee{
+	  public void goToWork(){
+		  System.out.println("Another day at the office");
+	  }
+	  public void goofAround(){
+		  System.out.println("Better hope the boss doesn't see");
+	  }
+}
