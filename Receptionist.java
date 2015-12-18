1a5714bb1c63a0ac34eb7d403ea0e9125ac10373
elementfrvr <dakota.mitchem@siu.edu>
2015-12-18 01:31:03 -0600

    Create Receptionist.java

---
+++ Receptionist.java
@@ -0,0 +1,8 @@
+public class Receptionist {
+	public void answerPhone(){
+		System.out.println("Hello, How may I help you?");
+	}
+	public void scheduleAppointment(){
+		System.out.println("Will Tuesday the 8th work?");
+	}
+}
